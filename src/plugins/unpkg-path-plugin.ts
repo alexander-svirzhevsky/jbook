0c68943247b7d1442d4fcb65d875a778321e243d
Alexander Svirzhevsky <alexander.svirzhevsky@gmail.com>
2022-03-30 21:30:23 +0300

    update onResolve function

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,32 +1,49 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
-    return {
-        name: "unpkg-path-plugin",
-        setup(build: esbuild.PluginBuild) {
-            build.onResolve({ filter: /.*/ }, async (args: any) => {
-                console.log("onResolve", args);
-                return { path: args.path, namespace: "a" };
-            });
+	return {
+		name: "unpkg-path-plugin",
+		setup(build: esbuild.PluginBuild) {
+			build.onResolve({ filter: /.*/ }, async (args: any) => {
+				console.log("onResolve", args);
+				if (args.path === "index.js") {
+					return { path: args.path, namespace: "a" };
+				}
 
-            build.onLoad({ filter: /.*/ }, async (args: any) => {
-                console.log("onLoad", args);
+				if (args.path.includes("./" || args.path.includes("../"))) {
+					return {
+						namespace: "a",
+						path: new URL(args.path, args.importer + "/").href
+					}
+				}
 
-                if (args.path === "index.js") {
-                    return {
-                        loader: "jsx",
-                        contents: `
-                                    import message from "./message"
-                                    console.log(message)
-                                `,
-                    };
-                } else {
-                    return {
-                        loader: "jsx",
-                        contents: "export default 'hi there' "
-                    }
-                }
-            });
-        },
-    };
-};
+				return {
+					namespace: "a",
+					path: `https://unpkg.com/${args.path}`
+				}
+			});
+
+			build.onLoad({ filter: /.*/ }, async (args: any) => {
+				console.log("onLoad", args);
+
+				if (args.path === "index.js") {
+					return {
+						loader: "jsx",
+						contents: `
+							const message = require("medium-test-pkg")
+							console.log(message)
+						`,
+					};
+				}
+
+				const { data } = await axios.get(args.path)
+				return {
+					loader: "jsx",
+					contents: data
+				}
+
+			});
+		},
+	};
+}; 
