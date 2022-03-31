74546a9a92ca3e11137dcc02ccc505d033bca7b4
Aliaksandr Svirzheyski <aliaksandr.svirzheyski@prezzee.com>
2022-03-31 14:41:02 +0300

    upd path

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,6 @@
 import * as esbuild from "esbuild-wasm";
 import axios from "axios";
+import { request } from "http";
 
 export const unpkgPathPlugin = () => {
 	return {
@@ -14,7 +15,7 @@ export const unpkgPathPlugin = () => {
 				if (args.path.includes("./" || args.path.includes("../"))) {
 					return {
 						namespace: "a",
-						path: new URL(args.path, args.importer + "/").href
+						path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/").href
 					}
 				}
 
@@ -31,16 +32,17 @@ export const unpkgPathPlugin = () => {
 					return {
 						loader: "jsx",
 						contents: `
-							const message = require("medium-test-pkg")
+							const message = require("react-dom")
 							console.log(message)
 						`,
 					};
 				}
 
-				const { data } = await axios.get(args.path)
+				const { data, request } = await axios.get(args.path)
 				return {
 					loader: "jsx",
-					contents: data
+					contents: data,
+					resolveDir: new URL("./", request.responseURL).pathname
 				}
 
 			});
